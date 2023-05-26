5333355e0a6f79e2614b6051121031e07f805ce2
sakotaka <134421966+sakotaka@users.noreply.github.com>
2023-05-26 20:29:24 +0900

    first commit.
    チーズアカデミー練習です。

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.facilities-container').slick({
+        slidesToScroll: 1,
+        slidesToShow: 3,
+        dots: true,
+        infinite: true,
+        speed: 500,
+        fade: false,
+        cssEase: 'linear',
+    });
+});
